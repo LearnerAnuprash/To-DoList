57c31e6d525836dd2501d82a88382be2becd0182
LearnerAnuprash <info.anuprash@gmail.com>
2025-06-08 23:49:46 +0545

    Added real-time in website

---
+++ logic.js
@@ -0,0 +1,33 @@
+let hour=document.getElementById("hourBox"); // gets element by ID
+let minute=document.getElementById("minuteBox");
+let second=document.getElementById("secondBox");
+let count=1;
+
+
+    function updateTime(){
+
+     let date=new Date();
+     let hours=date.getHours();
+    let minutes=date.getMinutes();
+    let seconds=date.getSeconds();
+
+    // Manipulation using DOM
+
+    
+    hour.innerHTML=String(hours).padStart(2,'0');  /*  String(hours) :  converts hours to string
+
+                                                       padStart(2,'0'): adds padding of 0 unless
+                                                                        the no. of characters become
+                                                                        two                */ 
+    minute.innerHTML=String(minutes).padStart(2,'0');
+
+    second.innerHTML=String(seconds).padStart(2,'0');
+
+}
+
+
+ 
+updateTime(); //Loads time immediately without delay
+
+
+setInterval(updateTime,1000);
